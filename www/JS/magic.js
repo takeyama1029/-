1276068a7d5e4fcc75f243169b09d655b9c7a0cf
takeyama1029 <shosen1029take@gmail.com>
2018-01-17 16:45:25 +0900

    Project's Initial Commit

---
+++ www/JS/magic.js
@@ -0,0 +1,75 @@
+function magic0(){
+    location.href = "1magic.html";
+}
+
+function magic1(){
+    var x = Math.random();
+    if(x <= 0.8){
+        location.href = "magic.html";
+    }else{
+        location.href = "ryuunen.html";
+    }
+}
+
+function magic2(){
+    var x = Math.random();
+    if(x <= 0.9){
+        location.href = "2magic.html";
+    }else{
+        location.href = "ryuunen2.html";
+    }
+}
+
+function magic3J(){
+    var x = Math.random();
+    if(x <= 0.8){
+        location.href = "3magicJ.html";
+    }else{
+        location.href = "ryuunen3J.html";
+    }
+}
+
+function magic3R(){
+    var x = Math.random();
+    if(x <= 0.8){
+        location.href = "3magicR.html";
+    }else{
+        location.href = "ryuunen3R.html";
+    }
+}
+
+function magic4J(){
+    var x = Math.random();
+    if(x <= 0.7){
+        location.href = "4magicJ.html";
+    }else{
+        location.href = "ryuunen4J.html";
+    }
+}
+
+function magic4R(){
+    var x = Math.random();
+    if(x <= 0.7){
+        location.href = "4magicR.html";
+    }else{
+        location.href = "ryuunen4R.html";
+    }
+}
+
+function magic5J(){
+    var x = Math.random();
+    if(x <= 0.6){
+        location.href = "5magicJ.html";
+    }else{
+        location.href = "ryuunen5J.html";
+    }
+}
+
+function magic5R(){
+    var x = Math.random();
+    if(x <= 0.6){
+        location.href = "5magicR.html";
+    }else{
+        location.href = "ryuunen5R.html";
+    }
+}
